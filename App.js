a6a60a03b8a08c112d8c522c7b5dd16b14745fdf
unknown <milja.haiko@gmail.com>
2023-01-25 13:57:18 +0200

    Tehtävää korjattu

--- App.js
+++ App.js
@@ -1,35 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
+let arvottu;
+let lkm;
 
 export default function App() {
 
-  const arvottu = Math.floor(Math.random() * 100) + 1;
-  var lkm =1;
+  const [input, setInput] =useState ('');
+  const [teksti, setTeksti] = useState('');
 
-  const [arvaus, setArvaus] =useState ('');
+  
+  const init = () => {
+    setTeksti('Guess a number between 1-100');
+    lkm= 0;
+    arvottu = Math.floor(Math.random() * 100) + 1;
+  }
 
-  const [teksti, setTeksti] = useState('Guess a number between 1-100');
+  useEffect(() => {
+    init();
+  }, [])
 
-  const buttonPressed = () => {
+  const makeGuess = () => {
+    const arvaus = Number(input);
+    lkm++;
     if (arvaus > arvottu) {
       setTeksti('Your guess ' + arvaus + ' is too high')
-      lkm = lkm + 1;
     } 
     else if (arvaus < arvottu){
       setTeksti('Your guess ' + arvaus + ' is too low')
-      lkm = lkm + 1;
     }
     else {
-      Alert.alert('You guessed the number in ' + lkm + ' guesses')
+      Alert.alert('You guessed the number in ' + lkm + ' guesses');
+      init();
     }
-  }
+    setInput('');
+  }    
+    
+  
 
   return (
     <View style={styles.container}>
       <Text>{teksti} </Text>
-      <TextInput style= {styles.input} keyboardType='number-pad' onChangeText={arvaus => setArvaus(arvaus)} value={arvaus} />
-      <Button onPress={buttonPressed} title='Make guess'/>
+      <TextInput style= {styles.input} keyboardType='number-pad' onChangeText={text => setInput(text)} value={input} />
+      <Button onPress={makeGuess} title='Make guess'/>
       <StatusBar style="auto" />
     </View>
   );
@@ -40,11 +54,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   input : {
     width:200  , 
     borderColor: 'gray', 
-    borderWidth: 1
+    borderWidth: 1,
+    margin: 10,
   },
 });
